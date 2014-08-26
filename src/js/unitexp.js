0019285bd485dd05c2c0a4ff71a1f3f5f1f6f3cf
muhiro <muhiro@usafusa.jp>
2014-08-26 21:19:52 +0900

    テストとかそのへん

--- src/js/unitexp.js
+++ src/js/unitexp.js
@@ -1,24 +1,25 @@
 /**
  * Created by muhiro on 2014/08/25.
  */
-var unitexp = unitexp || {};
+var unitExp = unitExp || {};
 (function() {
   var db = new aigisWidget.aigisdb();
   var exp = db.getUnitexp();
+  var grows = ['normal','cc','awake'];
 
-  unitexp.getMaxLevel = function(rarity, grow) {
+  unitExp.getMaxLevel = function(rarity, grow) {
     return exp.meta[rarity][grow];
   };
 
-  unitexp.getAmountExp = function(rarity, level) {
+  unitExp.getAmountOfExperience = function(rarity, level) {
     var v = 0;
-    if (exp.meta[rarity].level.length > level) {
+    if (exp.meta[rarity].level.length >= level) {
       v = exp.meta[rarity].level[Number(level) - 1];
     }
     return v;
   };
 
-  unitexp.getNextLevelExp = function(rarity, level) {
+  unitExp.getNextLevelExperience = function(rarity, level) {
     var v1 = 0;
     var v2 = 0;
     if (exp.meta[rarity].level.length > level) {
@@ -27,4 +28,21 @@ var unitexp = unitexp || {};
     }
     return v2 - v1;
   };
+
+  unitExp.getMaxLevelExperienceNeeded = function(rarity, nowgrow, level, exp, nextgrow, nextlevel) {
+    var idx = grows.indexOf(nowgrow);
+    var nidx = grows.indexOf(nextgrow);
+    var sum = unitExp.getAmountOfExperience(rarity, level) + exp;
+    for (var i = idx; i <= nidx; i++) {
+      var lvl = 0;
+      if ( (nextlevel === undefined) || (i != nidx) ) {
+        lvl = unitExp.getMaxLevel(rarity, grows[i]);
+      } else {
+        lvl = nextlevel;
+      }
+      sum += unitExp.getAmountOfExperience(rarity, lvl);
+    }
+    return sum;
+  };
+
 })();
